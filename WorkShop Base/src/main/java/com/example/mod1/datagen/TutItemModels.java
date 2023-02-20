ed48e743cc5d7a1266ccae84572d2bb133c7e2ba
Enzo Vignal <evignal@localhost.localdomain>
2023-02-20 15:23:37 +0100

    Workshop

---
+++ WorkShop Base/src/main/java/com/example/mod1/datagen/TutItemModels.java
@@ -0,0 +1,17 @@
+package com.example.mod1.datagen;
+
+import com.example.mod1.Mod1;
+import com.example.mod1.setup.Registration;
+import net.minecraft.data.DataGenerator;
+import net.minecraftforge.client.model.generators.ItemModelProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+
+public class TutItemModels extends ItemModelProvider {
+    public TutItemModels(DataGenerator generator, ExistingFileHelper existingFileHelper) {
+        super(generator, Mod1.MODID, existingFileHelper);
+    }
+
+    @Override
+    protected void registerModels() {
+    }
+}
---
+++ WorkShop Base/src/main/java/com/example/mod1/datagen/TutItemTags.java
@@ -0,0 +1,22 @@
+package com.example.mod1.datagen;
+
+import com.example.mod1.Mod1;
+import com.example.mod1.setup.Registration;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.tags.BlockTagsProvider;
+import net.minecraft.data.tags.ItemTagsProvider;
+import net.minecraftforge.common.Tags;
+import net.minecraftforge.common.data.ExistingFileHelper;
+
+public class TutItemTags extends ItemTagsProvider {
+    public TutItemTags(DataGenerator generator, BlockTagsProvider blockTags, ExistingFileHelper helper) {
+        super (generator, blockTags, Mod1.MODID, helper);
+    }
+
+    @Override
+    protected void addTags() {
+    }
+
+    @Override
+    public String getName() { return "Workshop Tags"; }
+}
