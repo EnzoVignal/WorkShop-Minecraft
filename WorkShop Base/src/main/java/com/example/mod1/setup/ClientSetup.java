1353e6d11f9f67e2a7d45ca01c1cd1e282eb57d6
Enzo Vignal <evignal@localhost.localdomain>
2023-02-27 13:28:51 +0100

    Modif archi

--- WorkShop Base/src/main/java/com/example/mod1/setup/ClientSetup.java
+++
@@ -1,9 +0,0 @@
-package com.example.mod1.setup;
-
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-
-public class ClientSetup {
-    public static void init(FMLClientSetupEvent event) {
-
-    }
-}
